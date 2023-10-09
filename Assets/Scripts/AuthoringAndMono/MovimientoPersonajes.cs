ee71b3bdf95190e87cad269790c97d1cbd46135e
CristianFelipe29 <pipebustos54@gmail.com>
2023-10-09 18:52:38 -0500

    a

--- Assets/Scripts/AuthoringAndMono/MovimientoPersonajes.cs
+++ Assets/Scripts/AuthoringAndMono/MovimientoPersonajes.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 public class MovimientoPersonajes : MonoBehaviour
 {
     public Transform objetivo; // El objetivo a seguir (romanos o aliens)
@@ -12,9 +11,19 @@ public class MovimientoPersonajes : MonoBehaviour
         Vector3 direccion = (objetivo.position - transform.position).normalized;
 
         // Mueve el personaje hacia el objetivo
-        transform.Translate( direccion * velocidad * Time.deltaTime);
+        transform.Translate(direccion * velocidad * Time.deltaTime);
 
         // Rota el personaje hacia la dirección del objetivo (opcional)
         transform.LookAt(objetivo);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Verifica si choca con el objeto objetivo
+        if (collision.transform == objetivo)
+        {
+            // Desaparece el objeto actual
+            Destroy(gameObject);
+        }
     }
 }
