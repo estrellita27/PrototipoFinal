35f63e4f31b645f81093f9a68342e0eacd80e5bb
CristianFelipe29 <pipebustos54@gmail.com>
2023-10-10 21:50:41 -0500

    1111

--- Assets/Scripts/AuthoringAndMono/MovimientoPersonajes.cs
+++ Assets/Scripts/AuthoringAndMono/MovimientoPersonajes.cs
@@ -58,14 +58,4 @@ public class MovimientoPersonajes : MonoBehaviour
             gameObject.SetActive(false);
         }
     }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        // Verifica si choca con el objeto objetivo
-        if (collision.transform == objetivo)
-        {
-            // Desaparece el objeto actual
-            Destroy(gameObject);
-        }
-    }
 }
